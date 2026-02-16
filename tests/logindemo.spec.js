48493c7d226e6103335a470acfaaf3bfe0fd84b4
Jnanodhay <jnanodhaya.jnanirj@gmail.com>
2026-02-16 23:44:37 +0530

    this is correct

--- tests/logindemo.spec.js
+++ tests/logindemo.spec.js
@@ -31,9 +31,7 @@ test('Login with invalid username and valid password', async ({ page }) => {
 
 test('Login with invalid username and invalid password', async ({ page }) => {
   await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
-  await page.getByRole('textbox', { name: 'Username' }).click();
   await page.getByRole('textbox', { name: 'Username' }).fill('Admin');
-  await page.getByRole('textbox', { name: 'Password' }).click();
   await page.getByRole('textbox', { name: 'Password' }).fill('gfgdfg');
   await page.getByRole('button', { name: 'Login' }).click();
 });
