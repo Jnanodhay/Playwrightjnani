ff0f89357b683336b3d591343cb4e4a17ded184a
Jnanodhay <jnanodhaya.jnanirj@gmail.com>
2026-02-25 22:37:29 +0530

    ins fake

---
+++ fakerTest.js
@@ -0,0 +1,4 @@
+import { faker } from '@faker-js/faker';
+
+console.log(faker.person.fullName());
+console.log(faker.internet.email());
--- tests/addempstatus.spec.js
+++ tests/addempstatus.spec.js
@@ -1,16 +1,17 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
+test('Verify Admin can add job title', async ({ page }) => {
   await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
 
-  // Expect a title "to contain" a substring.
-  await page.locator("input[name='username']").fill('Admin')
-  await page.locator("input[name='password']").fill('admin123')
-  await page.locator ("button[type='submit']").click()  // 
-//  await page.close() // this command uses to close the page atomatically
-  await page.locator (" ")
+  await page.getByRole('textbox', { name: 'Username' }).fill('Admin');
 
+  await page.getByRole('textbox', { name: 'Password' }).fill('admin123');
 
-
-
+  await page.waitForTimeout(10000)
+  await page.getByRole('button', { name: 'Login' }).click();
+  await expect(page.getByRole('heading', { name: 'Dashboard' })).toBeVisible();
+  await page.getByRole('link', { name: 'Admin' }).click({timeout: 50000});
+  await page.getByText('Job', { exact: true }).click();
+  await page.getByRole('menuitem', { name: 'Job Titles' }).click();
+  await page.getByRole('button', { name: ' Add' }).click();
 });
