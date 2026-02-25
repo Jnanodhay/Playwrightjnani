6a1c9a8e54ffe4e02b41faecc5aac79fab2d42b4
Jnanodhay <jnanodhaya.jnanirj@gmail.com>
2026-02-25 22:08:09 +0530

    book

--- tests/addempstatus.spec.js
+++ tests/addempstatus.spec.js
@@ -7,5 +7,10 @@ test('has title', async ({ page }) => {
   await page.locator("input[name='username']").fill('Admin')
   await page.locator("input[name='password']").fill('admin123')
   await page.locator ("button[type='submit']").click()  // 
-  await page.close() // this command uses to close the page atomatically
+//  await page.close() // this command uses to close the page atomatically
+  await page.locator (" ")
+
+
+
+
 });
